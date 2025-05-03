f5dc19634a5041682e79f9a1b4c09e848fde40f5
AntonCanzobre <anton.c744@gmail.com>
2025-05-03 10:15:20 +0200

    fix(camera): Camera

--- HackConnectWeb/src/pages/ScanPage.jsx
+++ HackConnectWeb/src/pages/ScanPage.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import QRScanner from '../components/QRScanner';
 import Header from '../components/static/Header';
